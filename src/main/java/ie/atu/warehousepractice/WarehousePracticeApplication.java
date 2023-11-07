674d5f346e69d30905b77a11480e696efdfddbf3
Mark Veerasingam <markveer70@gmail.com>
2023-11-07 21:31:39 +0000

    first commit

---
+++ src/main/java/ie/atu/warehousepractice/WarehousePracticeApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.warehousepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehousePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehousePracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/warehousepractice/WarehousePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.warehousepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehousePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
