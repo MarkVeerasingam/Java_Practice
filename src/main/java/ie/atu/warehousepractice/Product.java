35ad70db87e280bceb6511d2178ef8f090d48d9b
Mark Veerasingam <markveer70@gmail.com>
2023-11-07 21:36:12 +0000

    added product class

---
+++ src/main/java/ie/atu/warehousepractice/Product.java
@@ -0,0 +1,15 @@
+package ie.atu.warehousepractice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private int productId;
+    private String productName;
+    private int price;
+    private int quantity;
+}
